d21bee21ff3f8ae13ce4eb2121790566a3488362
snflk620 <anna.m.mckee@gmail.com>
2023-09-27 15:32:04 -0400

    working alignment code with printout

--- aligned.py
+++ aligned.py
@@ -3,27 +3,26 @@
 #open file of interest
 file = open("aligned.fna")
 
-#read every other line in the fna file starting after the first header
-sequences = repr(file.readlines()[1::2])
-
+#read every other line in the fna file starting after the first header -> returns a list
+fin = file.readlines()[1::2]
 
 #close file to clear memory
 file.close()
 
-#print contents as a string to view whitespace
-# print(repr(contents.split("\n")))
-# sequences = contents.readlines()[1::2]
-print(sequences)
+#create an empty list, populate the list with sequences with newline stripped from the ends
+sequences = []
+for seq in fin:
+    sequences.append(seq.strip())
 
+#create an empty string variable for the matches
+#iterate over each position in the sequences, if they match add | to the string, if not, add a space
+match = ""
+for i in range(len(sequences[0])):
+    if sequences[0][i] == sequences[1][i]:
+        match += "|"
+    else:
+        match += " "
 
-#create a string variable for each sequence
-#
-#create an empty variable
-#iterate across the sequences, when there's a nucletide match, print | to the empty seq, when there's no match print " "
-#print 1 seq, then match string, then other seq
-
-
-
-#asldkjlkj
-x = 6
-print(x)
+#print the first sequence, followed by the match string, followed by the second sequence
+#use + instead of , to seperate items to print to avoid adding a space before match and sequences
+print(sequences[0] + "\n" + match + "\n" + sequences[1])
